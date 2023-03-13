fa127a0bc5622ed5ab5095ac769514797ad13096
Hrittik Das <hrittikdas16@gmail.com>
2023-03-13 21:26:47 +0530

    .gitignore added, perceptron.py modified, readme modified

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
-# Perceptron
+# Single Layer Perceptron
 
-#### Building a simple perceptron that can classify bmi of a person into weight classes
+#### Building a simple single layer perceptron that can classify bmi of a person into healthy or unhealthy
 
 ### BMI = Weight(Kgs) / Height<sup>2</sup>(m)
 
-#### A simple perceptron with no hidden layers can learn to classify bmi into 4 different weight classes: Underweight(<18.5 BMI), Healthy(18.5 - 24.9 BMI), Overweight(25.0 - 29.0 BMI) and Obese(>30.0 BMI)
+#### There are four categories: Underweight(<18.5 BMI), Healthy(18.5 - 24.9 BMI), Overweight(25.0 - 29.0 BMI) and Obese(>30.0 BMI)
+
+#### Underweight, overweight and obese will be classified as unhealthy
--- perceptron.py
+++ perceptron.py
@@ -4,7 +4,7 @@ import random
 
 def random_bmi_generator():
 	bmi = []
-	for i in range(1000):
+	for i in range(2500):
 		overweight_bmi = random.uniform(25.0, 29.9)
 		obese_bmi = random.uniform(30.0, 50.0)
 		bmi.append(overweight_bmi)
@@ -23,16 +23,11 @@ def calculate_bmi():
 	# convert pounds to kilogram (1 lb = 0.45359237 kgs)
 	for i in range(df['Weight(Pounds)'].size):
 		df.at[i, 'Weight(Pounds)'] = df['Weight(Pounds)'][i]*0.45359237
-	
-	# print(df['Height(Inches)'])
-	# print(df['Weight(Pounds)'])
 
 	# rename the columns
 	df.rename(columns={'Height(Inches)': 'Height(m)',
 					   'Weight(Pounds)': 'Weight(Kgs)'},
 					   inplace=True)
-
-	# print(df.head(n = 20))
 
 	# calculate bmi (bmi = weight(in kgs)/height^2(in m))
 	bmi = []
@@ -51,23 +46,18 @@ def calculate_bmi():
 	# add weight categories to bmi
 	for i in range(len(bmi)):
 		if bmi[i] < 18.5:
-			category.append("Underweight")
+			category.append("Unhealthy")
 		elif bmi[i] < 25.0:
 			category.append("Healthy")
-		elif bmi[i] < 30.0:
-			category.append("Overweight")
 		else:
-			category.append("Obese")
+			category.append("Unhealthy")
 
 	# write bmi and category to bmi.csv
 	new_df = pd.DataFrame()
 	new_df['BMI']  = bmi
 	new_df['CATEGORY'] = category
 	new_df.to_csv('data/bmi.csv')
-
-	print(new_df.head(n=20))
 	
-
 
 if __name__ == "__main__":
 	calculate_bmi()
